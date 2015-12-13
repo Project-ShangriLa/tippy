ffaef05f7bd271f5e4199183d2b45386f5311d17
AKB428 <gandara016+1@gmail.com>
2015-12-14 03:38:45 +0900

    add action

--- tippy.rb
+++ tippy.rb
@@ -4,7 +4,12 @@ require 'haml'
 # 静的コンテンツ参照のためのパス設定
 set :public, File.dirname(__FILE__) + '/public'
 
-# アップロード
 get '/' do
   haml :index
+end
+
+post '/inputid' do
+  @id = params[:inputId]
+  p @id
+  haml :result
 end
