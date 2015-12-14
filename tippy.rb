fd974b4807704aa8856fdf6fceed12987222a88f
AKB428 <gandara016+1@gmail.com>
2015-12-14 23:22:46 +0900

    fix

--- tippy.rb
+++ tippy.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'haml'
+require 'shangrila'
 
-# 静的コンテンツ参照のためのパス設定
-set :public, File.dirname(__FILE__) + '/public'
+configure do
+  # 静的コンテンツ参照のためのパス設定
+  set :public, File.dirname(__FILE__) + '/public'
+
+  puts "-------LOAD-------"
+  set(:master) { Shangrila::Sora.new().get_map_key_id(2015, 4) }
+end
 
 get '/' do
   haml :index
@@ -11,5 +17,6 @@ end
 post '/inputid' do
   @id = params[:inputId]
   p @id
+  #settings.master
   haml :result
 end
