b9572beed63ad77cd79ebb196db53f724e424d39
AKB428 <gandara016+1@gmail.com>
2015-12-15 23:40:05 +0900

    fix

--- tippy.rb
+++ tippy.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'haml'
 require 'shangrila'
+require 'sequel'
 
 configure do
   # 静的コンテンツ参照のためのパス設定
@@ -16,7 +17,28 @@ end
 
 post '/inputid' do
   @id = params[:inputId]
-  p @id
+  twitter_name =  @id
   #settings.master
+  DB = Sequel.mysql2('tippy1', :host=>'localhost', :user=>'root', :password=>'', :port=>'3306')
+
+  id = DB[:twitter_user].filter(:name => twitter_name).select(:id).first
+
+  if id.nil?
+    puts '---- ID NO MATCH ----'
+    haml :result
+    return
+  end
+
+  puts '---- ID MATCH ----'
+
+  # 全部出す
+  @cf_data =  DB[:cf_input_rating].filter(:id => id[:id]).order(:rate).reverse.all
+
+  @prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).reverse.limit(10).all
+
+  @un_prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).limit(5).all
+
+  #TODO shangrila-masterと結合
+
   haml :result
 end
