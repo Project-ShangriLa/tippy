e8addc9c9202b92869a87d2c9f37b8f4769fface
AKB428 <gandara016+1@gmail.com>
2015-12-16 01:10:34 +0900

    fix haml

--- tippy.rb
+++ tippy.rb
@@ -18,27 +18,22 @@ end
 post '/inputid' do
   @id = params[:inputId]
   twitter_name =  @id
-  #settings.master
+  @master = settings.master
+  @cf_data = []
+  @prediction = []
+  @un_prediction = []
+
   DB = Sequel.mysql2('tippy1', :host=>'localhost', :user=>'root', :password=>'', :port=>'3306')
 
   id = DB[:twitter_user].filter(:name => twitter_name).select(:id).first
 
-  if id.nil?
-    puts '---- ID NO MATCH ----'
-    haml :result
-    return
+  if id != nil
+    @cf_data =  DB[:cf_input_rating].filter(:id => id[:id]).order(:rate).reverse.all
+
+    @prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).reverse.limit(10).all
+
+    @un_prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).limit(5).all
   end
-
-  puts '---- ID MATCH ----'
-
-  # 全部出す
-  @cf_data =  DB[:cf_input_rating].filter(:id => id[:id]).order(:rate).reverse.all
-
-  @prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).reverse.limit(10).all
-
-  @un_prediction = DB[:prediction_csv].filter(:id => id[:id]).order(:rate).limit(5).all
-
-  #TODO shangrila-masterと結合
 
   haml :result
 end
