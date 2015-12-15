324b2d9b06f3e9bae3b8f60386fe04e05e734f8b
AKB428 <gandara016+1@gmail.com>
2015-12-16 04:01:27 +0900

    fix DDL

--- DDL/cf_input_rating.sql
+++ DDL/cf_input_rating.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `prediction` (
-	  `id` int(11) NOT NULL,
-	  `product_id` int(11) NOT NULL,
-	  `rate` double NOT NULL,
-	  PRIMARY KEY (`id`,`product_id`),
-	  KEY `prediction_id` (`id`),
-	  KEY `prediction_product_id` (`product_id`),
-	  KEY `prediction_rate` (`rate`)
+CREATE TABLE `cf_input_rating` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `rate` double NOT NULL,
+  PRIMARY KEY (`id`,`product_id`),
+  KEY `cf_input_rating_id` (`id`),
+  KEY `cf_input_rating_product_id` (`product_id`),
+  KEY `cf_input_rating_rate` (`rate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- DDL/csv_to_innoDB.sql
+++ DDL/csv_to_innoDB.sql
@@ -1,2 +1,3 @@
+FLUSH TABLES prediction_csv
 ALTER TABLE prediction_csv ENGINE InnoDB;
 ALTER TABLE prediction_csv ADD INDEX prediction_id_index(id);
